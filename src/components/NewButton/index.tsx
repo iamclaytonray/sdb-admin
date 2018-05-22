5083b6397844f4fb989d8748eb629e8698ea7514
Clayton Ray <iamclaytonray@gmail.com>
2018-05-22 09:53:11 -0400

    refactor: remove inline styles on NewButton

--- src/components/NewButton/index.tsx
+++ src/components/NewButton/index.tsx
@@ -3,13 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const NewButton = ({ location, children, ...props }) => {
   return (
-    <div
-      style={{
-        textAlign: 'right',
-        marginTop: 30,
-        marginBottom: 50,
-      }}
-    >
+    <div>
       <Link to={location}>
         <button className="btn btn-primary">{children}</button>
       </Link>
