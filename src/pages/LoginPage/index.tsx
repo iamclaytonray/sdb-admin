c9c2d198e491ef1f1a3b6b20e33b95557a0b6593
Clayton Ray <iamclaytonray@gmail.com>
2018-06-04 21:07:31 -0400

    layout: add more structure to LoginPage

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,11 +1,26 @@
+import { CustomButton as Button } from 'components/CustomButton';
 import * as React from 'react';
-import { Card, CardTitle } from 'reactstrap';
+import { Card, CardBody, CardTitle } from 'reactstrap';
 
 export const LoginPage = () => {
   return (
     <Card>
-      <CardTitle>Login</CardTitle>
-      <input name="" defaultValue="username" />
+      <CardBody>
+        <CardTitle>Login</CardTitle>
+        <input
+          className="form-control"
+          name=""
+          defaultValue="shoreshdavidbrandon"
+        />
+        <br />
+        <input
+          className="form-control"
+          name="password"
+          placeholder="Password"
+          defaultValue="password"
+        />
+        <Button className="btn btn-primary">Login</Button>
+      </CardBody>
     </Card>
   );
 };
