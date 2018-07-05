bc04d49d31955b79dabfd3b4b1217ee155de3acf
Clayton Ray <iamclaytonray@gmail.com>
2018-07-05 11:43:45 -0400

    improv: update layout on DashboardPage

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
-import { Card } from 'reactstrap';
+import { Card, CardBody, CardText, CardTitle, Col } from 'reactstrap';
 
 export const DashboardPage = () => {
   return (
     <Card>
-      <h1>Dashboard</h1>
+      <CardBody>
+        <CardTitle>Dashboard</CardTitle>
+        {/*  */}
+        <Col lg="3">
+          <Card>
+            <CardBody>
+              <CardText>Articles</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        {/*  */}
+        <Col lg="3">
+          <Card>
+            <CardBody>
+              <CardText>Media</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        {/*  */}
+        <Col lg="3">
+          <Card>
+            <CardBody>
+              <CardText>Products</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        {/*  */}
+      </CardBody>
     </Card>
   );
 };
